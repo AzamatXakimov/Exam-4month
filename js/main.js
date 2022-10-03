8c154a2c8afda5abcf81c8a716b01ba5e3c52ee1
AzamatXakimov <typetiger59@gmail.com>
2022-10-03 22:20:38 +0500

    Finish section

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ var elNavBarBtn = document.querySelector(".js-navbar-btn");
 elNavBarBtn.addEventListener("click", function(){
     document.body.classList.toggle("navbar-show");
 })
+
+$('.corusel-responsive').slick({
+    centerMode: true,
+    centerPadding: '10px',
+    slidesToShow: 2,
+    variableWidth: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+        {
+            breakpoint: 1000,
+            settings: {
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
